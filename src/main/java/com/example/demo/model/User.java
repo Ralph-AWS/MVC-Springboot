eb4b47a2558bd87eb9d1a23116c6979f58aa703f
ralph estonio <ralph.estonio@awsys-i.com>
2025-05-26 14:24:51 +0800

    fix: show validation in all field

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -33,26 +33,25 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-
-    @NotBlank(message = "First name is required. ")
+    @NotBlank(groups = {ValidationGroups.Create.class, ValidationGroups.Update.class}, message = "First name is required. ")
     @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @NotBlank(message = "Last name is required. ")
+    @NotBlank(groups = {ValidationGroups.Create.class, ValidationGroups.Update.class}, message = "Last name is required. ")
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
-    @NotNull(message = "Date of birth is required.")
+    @NotNull(groups = {ValidationGroups.Create.class, ValidationGroups.Update.class}, message = "Date of birth is required.")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Column(name = "dob", nullable = false)
     private LocalDate dob;
 
-    @NotBlank(message = "Gender is required. ")
+    @NotBlank(groups = {ValidationGroups.Create.class, ValidationGroups.Update.class}, message = "Gender is required. ")
     @Column(name = "gender", nullable = false)
     private String gender;
 
-    @NotBlank(message = "Username is required. ")
-    @Size(min = 4, message = "Username must be at least 4 characters")
+    @NotBlank(groups = {ValidationGroups.Create.class, ValidationGroups.Update.class}, message = "Username is required. ")
+    @Size(min = 4, groups = {ValidationGroups.Create.class, ValidationGroups.Update.class}, message = "Username must be at least 4 characters")
     @Column(name = "username", unique = true, nullable = false)
     private String username;
 
@@ -61,18 +60,18 @@ public class User {
     @Column(name = "password", nullable = false)
     private String password;
 
-    @NotBlank(message = "Email is required. ")
-    @Email(message = "Invalid email")
+    @NotBlank(groups = {ValidationGroups.Create.class, ValidationGroups.Update.class}, message = "Email is required. ")
+    @Email(groups = {ValidationGroups.Create.class, ValidationGroups.Update.class}, message = "Invalid email")
     @Column(name = "email", unique = true, nullable = false)
     private String email;
 
-    @NotBlank(message = "Phone number is required. ")
-    @Pattern(regexp = "^\\d{10}$", message = "Phone number must be 10-digit")
+    @NotBlank(groups = {ValidationGroups.Create.class, ValidationGroups.Update.class}, message = "Phone number is required. ")
+    @Pattern(regexp = "^\\d{10}$", groups = {ValidationGroups.Create.class, ValidationGroups.Update.class}, message = "Phone number must be 10-digit")
     @PhoneNumber
     @Column(name = "phone", nullable = false)
     private String phoneNumber;
 
-    @AssertTrue(message = "You must agree to the terms and conditions")
+    @AssertTrue(groups = {ValidationGroups.Create.class, ValidationGroups.Update.class}, message = "You must agree to the terms and conditions")
     @Column(name = "terms", nullable = false)
     private boolean termsAndConditions;
 
